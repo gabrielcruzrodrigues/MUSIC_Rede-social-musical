1fbd7164f07db465ad185a6893243917b404c285
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-11-01 11:27:42 -0300

    first commit

---
+++ src/test/java/com/gabriel/music/redesocial/RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.music.redesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
